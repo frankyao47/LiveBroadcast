6ff96d4f1e53517b4f2476d41b0088524fff5a8e
Frank Yao <frankyao47@gmail.com>
2017-01-23 11:06:46 +0800

    js分文件

---
+++ client/static/js/player.js
@@ -0,0 +1,54 @@
+function getParamsFromMeta(name) {
+    return $('meta[name=' + name + ']').attr("content");
+}
+
+//url中获取播放器参数
+var rtmp = getParamsFromMeta('rtmp'),
+    hls = getParamsFromMeta('hls'),
+    live = true,
+    coverpic = getParamsFromMeta('coverpic'),
+    width = $('.video-pane-head').outerWidth(),
+    height = $('.video-pane-head').outerHeight(),
+    autoplay = true;
+/**
+ * 视频类型播放优先级
+ * mobile ：m3u8>mp4
+ * PC ：RTMP>flv>m3u8>mp4
+ */
+var options = {
+    rtmp: rtmp,
+    m3u8: hls,
+    coverpic: coverpic,
+    autoplay: autoplay ? true : false,
+    live: live,
+    width : width,
+    height : height,
+    wording: {
+        2032: '请求视频失败，请检查网络',
+        2048: '请求m3u8文件失败，可能是网络错误或者跨域问题'
+    },
+    listener: function (msg) {
+        //console.log(msg.type);
+    }
+};
+
+
+player = new TcPlayer('video-container', options);
+console.log(player);
+
+//全屏后关闭弹幕
+function checkFullScreen() {
+    if (player.fullscreen()) {
+        $(".video-pane-body").hide();
+    }
+    else {
+        $(".video-pane-body").show();
+    }
+    setTimeout(function() { checkFullScreen(); }, 50);
+}
+checkFullScreen();
+
+//landscape全屏
+window.addEventListener("orientationchange", function() {
+   player.size($('.video-pane-head').outerWidth(), $('.video-pane-head').outerHeight());
+});
